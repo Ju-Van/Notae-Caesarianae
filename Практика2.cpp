c6de91e645ba32159d5d5ba62e0715c492f8fcfb
Ju-Van <152848878+Ju-Van@users.noreply.github.com>
2023-12-04 11:08:52 +0300

    Add files via upload

---
+++ Практика2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int i;
+    int k = 0;
+    char masiv[20];
+    cout << "Введите сообщение,которое будет закодировано:" << endl;
+    cin >> masiv;
+    cout << "Введите сдвиг:" << endl;
+    cin >> k;
+
+    cout << "Закодировать сообщение - нажмите \"1\". Раскодировать - нажмите \"2\"\n";
+    {
+        switch (_getch())
+        {
+        case '1':
+            for (i = 0; i < strlen(masiv); i++)
+                masiv[i] += k;
+            cout << "\nЗакодированное сообщение:\n\n" << masiv << endl << endl;
+        case '2':
+            for (i = 0; i < strlen(masiv); i++)
+                masiv[i] -= k;
+            cout << "\nРаскодированное сообщение:\n\n" << masiv << endl << endl;
+        }}
+    return 0;
+}
